dcba68d2062a2d043929dedfc74f58188bde8db7
MilnerLab <milnerlab@protonmail.com>
2026-02-10 15:12:33 -0800

    jet_only script makes nice plot now

--- manuscript_plotting_scripts/ocs/jet_only.py
+++ manuscript_plotting_scripts/ocs/jet_only.py
@@ -19,11 +19,18 @@ from base_core.math.models import Angle, Point, Range
 from base_core.plotting.enums import PlotColor
 from base_core.quantities.enums import Prefix
 from base_core.quantities.models import Length, Time
-print('Dependencies')
 
 
+
+#Path to save figure in
+fig_filedir = r"Z:\Droplets\plots" 
+fig_filename = fig_filedir + r"\\jet_only_TEMP.png" #Name the file to save here
+
+#Plot on top
+PlotTitle = r'OCS Jet with usCFG set to max/min acceleration (above/below)'
+
+#FIRST EXPERIMENT
 # GA=26, DA = 16.3mm
-
 configs: list[IonDataAnalysisConfig] = []
 folders: list[Path] = []
 
@@ -34,51 +41,107 @@ configs.append(IonDataAnalysisConfig(
     angle= Angle(12, AngleUnit.DEG),
     analysis_zone= Range[int](30, 90),
     transform_parameter= 0.73))
+folders_1 = folders
+configs_1 = configs
 
+#SECOND EXPERIMENT
+# GA=0, DA = 16.6mm
+configs: list[IonDataAnalysisConfig] = []
+folders: list[Path] = []
+
+folders.append(Path(r"20260207\Scan4"))
+configs.append(IonDataAnalysisConfig(
+    delay_center= Length(92.654, Prefix.MILLI),
+    center=Point(203, 202),
+    angle= Angle(12, AngleUnit.DEG),
+    analysis_zone= Range[int](30, 90),
+    transform_parameter= 0.73))
+folders_2 = folders
+configs_2 = configs
+
+#--------------------------------------------------------------------------------------------------
+
+#Prep for pipeline for first experiment
+folders = folders_1
+configs = configs_1
+#Pipeline start
 scans_paths = DatFinder(folders).find_datafiles()
-
 raw_datas = load_ion_data(scans_paths, configs)
-print('Data loaded!')
 save_path = create_save_path_for_calc_ScanFile(folders[0], str(raw_datas[0].ion_datas[0].run_id))
 calculated_scans = run_pipeline(raw_datas, save_path)
-
-TestIndex = 4 #Delay point
-plot_radius = 150
-h_shift, edgex, edgey  = raw_datas[0].ion_datas[TestIndex].get_2D_histogram(num_bins=2*plot_radius,xy_range=Range(-plot_radius,plot_radius))
-
-
 averagedScanData = average_scans(calculated_scans)
 config = StftAnalysisConfig(calculated_scans)
 resampled_scans = resample_scans(calculated_scans, config.axis)
 spectrogram = calculate_averaged_spectrogram(resampled_scans, config)
+#Pipeline end for first experiment
+plottable_scan_1 = averagedScanData
+plottable_spectrogram_1 = spectrogram
+plottable_nyquist_1 = calculated_scans[0]
+'''!!!'''
 
+#Prep for pipeline for second experiment
+folders = folders_2
+configs = configs_2
+#Pipeline start
+scans_paths = DatFinder(folders).find_datafiles()
+raw_datas = load_ion_data(scans_paths, configs)
+save_path = create_save_path_for_calc_ScanFile(folders[0], str(raw_datas[0].ion_datas[0].run_id))
+calculated_scans = run_pipeline(raw_datas, save_path)
+averagedScanData = average_scans(calculated_scans)
+config = StftAnalysisConfig(calculated_scans)
+resampled_scans = resample_scans(calculated_scans, config.axis)
+spectrogram = calculate_averaged_spectrogram(resampled_scans, config)
+#Pipeline end for second experiment
+plottable_scan_2 = averagedScanData
+plottable_spectrogram_2 = spectrogram
+plottable_nyquist_2 = calculated_scans[0]
 
-ionfig, (ax_shift) = plt.subplots(
-            nrows=1,
-            ncols=1,
-            figsize=(4, 4), 
-        )
+#Plot histogram to check centre (change variable here)
+if False:
+    TestIndex = 4 #Delay point
+    plot_radius = 150
+    h_shift, edgex, edgey  = raw_datas[0].ion_datas[TestIndex].get_2D_histogram(num_bins=2*plot_radius,xy_range=Range(-plot_radius,plot_radius))
 
-ax_shift.pcolor(edgex,edgey,h_shift)
-ax_shift.axis('equal')
+    #Create figures
+    ionfig, (ax_shift) = plt.subplots(
+                nrows=1,
+                ncols=1,
+                figsize=(4, 4), 
+            )
 
+    ax_shift.pcolor(edgex,edgey,h_shift)
+    ax_shift.axis('equal')
 
-fig, (axs) = plt.subplots(
+#Main figure
+mainfig, (axs) = plt.subplots(
             nrows=2,
             ncols=2,
             figsize=(10, 8),
-            sharex=True 
+            sharex=True, 
+            gridspec_kw={'hspace': 0,'wspace' : 0.275},
         )
 
+
+#Plot first experiment in top row
 a = axs[0,0]
-plot_averaged_scan(a, averagedScanData, PlotColor.GREEN, label=" -> CFG randomized")
-a.legend(loc="upper right")
-
-
+plot_averaged_scan(a, plottable_scan_1, PlotColor.BLUE,ecolor=PlotColor.RED)
+#a.legend(loc="upper right")
 a = axs[0,1]
-
-plot_Spectrogram(a, spectrogram)
+plot_Spectrogram(a, plottable_spectrogram_1)
 a.set_ylim([0,120])
-plot_nyquist_frequency(a, calculated_scans[0])
+plot_nyquist_frequency(a, plottable_nyquist_1)
 
+
+#Plot second experiment in bottom row
+a = axs[1,0]
+plot_averaged_scan(a, plottable_scan_2, PlotColor.BLUE,ecolor=PlotColor.RED)
+#a.legend(loc="upper right")
+a = axs[1,1]
+plot_Spectrogram(a, plottable_spectrogram_2)
+a.set_ylim([0,120])
+plot_nyquist_frequency(a, plottable_nyquist_2)
+
+mainfig.suptitle(PlotTitle)
+
+mainfig.savefig(fig_filename,format='png')
 plt.show()
