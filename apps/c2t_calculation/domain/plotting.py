c2601c995c08cc0a0ddb4ccb4b0db0c0e5be7eee
MilnerLab <milnerlab@protonmail.com>
2026-05-06 13:57:31 -0700

    update stft script for soeren to pull camerons-branch1

--- apps/c2t_calculation/domain/plotting.py
+++ apps/c2t_calculation/domain/plotting.py
@@ -3,14 +3,16 @@ import numpy as np
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 from _domain.plotting import plot_ScanData
 from base_core.lab_specifics.base_models import C2TScanData, Points
+from base_core.plotting.enums import PlotColor
+from base_core.quantities.enums import Prefix
 
 
 
-def plot_calculated_scan(ax: Axes, data: C2TScanData,label:str = '',*,number_of_scans: int = 1) -> None:
+def plot_calculated_scan(ax: Axes, data: C2TScanData,label:str = '',*,color=PlotColor.BLUE,ecolor=PlotColor.RED,number_of_scans: int = 1) -> None:
     #if data.file_path is not None:
         #label = f"{data.file_path.stem}"
     label =  f"{number_of_scans} scans" 
-    plot_ScanData(ax, data, label)
+    plot_ScanData(ax, data, label,color,ecolor)
 
 def plot_ions_square(
     ax: Axes,
